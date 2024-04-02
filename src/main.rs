d7cc252cf97e82377bc5c3f8ceb94fdb0840059a
Matt Palmer <palmer.s.matt@gmail.com>
2024-04-02 08:50:21 -0400

    Run whisper in a thread

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 mod audio;
 mod sync;
+mod whisper;
 
 use std::sync::{Arc, Condvar, Mutex};
 
 use anyhow::anyhow;
 
 use cpal::traits::{DeviceTrait, HostTrait};
-use whisper_rs::{FullParams, WhisperContext, WhisperError};
 use whisper_rs::install_whisper_log_trampoline;
 
-use crate::audio::input::{controlled_recording, Recording};
+use crate::{
+    audio::input::{controlled_recording, Recording},
+    sync::ProcessNode,
+    whisper::{Segment, Whisper},
+};
 
 #[derive(Debug, Clone)]
 struct Notifier<T: Clone>(Arc<(Mutex<T>, Condvar)>);
@@ -42,21 +46,36 @@ fn device_matching_name() -> Result<cpal::Device, anyhow::Error> {
 
     let device = host
         .input_devices()?
-        .find(|x| x.name().map(|x| x.contains("Microphone")).unwrap_or(false))
+        .find(|x| x.name().map(|x| x.contains("Buds")).unwrap_or(false))
         .ok_or(anyhow!("no input device available"))?;
 
     Ok(device)
 }
 
 fn main() -> Result<(), anyhow::Error> {
-    let device = device_matching_name()?;
     install_whisper_log_trampoline();
 
+    let device = device_matching_name()?;
     println!("{:?}", device.name()?);
 
     let p = sync::ProcessNode::new(|it| it.collect::<Vec<_>>());
 
-    let audio = {
+    let wh: ProcessNode<Vec<f32>, Result<Vec<Segment>, anyhow::Error>> =
+        sync::ProcessNode::new(|mut it| {
+            let whisper =
+                Whisper::new("/Users/matt/installations/whisper.cpp/models/ggml-base.en.bin")?;
+
+            let segments = it
+                .next()
+                .ok_or(anyhow!("no audio"))
+                .and_then(|audio| Ok(whisper.transcribe_audio(audio)?))?;
+
+            Ok(segments)
+        });
+
+    let (snd, hnd) = wh.run();
+
+    {
         let rec: Recording<_, Vec<_>> = controlled_recording(&device, p);
 
         let mut input = String::new();
@@ -64,64 +83,13 @@ fn main() -> Result<(), anyhow::Error> {
         rec.start();
 
         std::io::stdin().read_line(&mut input).unwrap();
-        rec.stop()?
-    };
+        let audio = rec.stop()?;
+        snd.send(audio)?;
+    }
 
-    println!("{:?}", audio.len());
-    let segments = transcribe_audio(audio)?;
+    let result = hnd.join().unwrap()?;
 
-    println!("{:?}", segments);
+    println!("{:?}", result);
 
     Ok(())
-}
-
-#[derive(Debug)]
-struct Segment {
-    start: usize,
-    end: usize,
-    text: String,
-}
-
-fn transcribe_audio<T>(data: T) -> Result<Vec<Segment>, anyhow::Error>
-where
-    T: AsRef<[f32]>,
-{
-    let ctx = whisper_context()?;
-    let mut state = ctx.create_state()?;
-
-    match state.full(
-        FullParams::new(whisper_rs::SamplingStrategy::Greedy { best_of: 1 }),
-        data.as_ref(),
-    ) {
-        Ok(0) => {}
-        Ok(n) => return Err(anyhow!("whisper exited with non-zero status: {n}")),
-        Err(e) => {
-            return Err(e.into());
-        }
-    };
-
-    let segments = state.full_n_segments()?;
-
-    (0..segments)
-        .map(|n| {
-            let text = state.full_get_segment_text(n)?;
-            let start = state.full_get_segment_t0(n)?;
-            let end = state.full_get_segment_t1(n)?;
-
-            Ok(Segment {
-                start: start.try_into()?,
-                end: end.try_into()?,
-                text,
-            })
-        })
-        .collect()
-}
-
-fn whisper_context() -> Result<WhisperContext, WhisperError> {
-    let mut params = whisper_rs::WhisperContextParameters::default();
-    params.use_gpu(true);
-    whisper_rs::WhisperContext::new_with_params(
-        "/Users/matt/installations/whisper.cpp/models/ggml-small.en.bin",
-        params,
-    )
 }
---
+++ src/whisper.rs
@@ -0,0 +1,84 @@
+use itertools::Itertools;
+use whisper_rs::{FullParams, WhisperContext, WhisperError};
+
+#[derive(Debug)]
+pub struct Segment {
+    start: usize,
+    end: usize,
+    text: String,
+}
+
+pub struct Whisper {
+    context: WhisperContext,
+}
+
+impl Whisper {
+    pub fn new(model_path: &str) -> Result<Self, WhisperError> {
+        let mut params = whisper_rs::WhisperContextParameters::default();
+        params.use_gpu(true);
+        let context = whisper_rs::WhisperContext::new_with_params(model_path, params)?;
+        Ok(Self { context })
+    }
+
+    pub fn create_state(&self) -> Result<whisper_rs::WhisperState, WhisperError> {
+        self.context.create_state()
+    }
+
+    pub fn transcribe_audio<T>(&self, data: T) -> Result<Vec<Segment>, WhisperError>
+    where
+        T: AsRef<[f32]>,
+    {
+        let mut state = self.create_state()?;
+
+        let mut params = FullParams::new(whisper_rs::SamplingStrategy::Greedy { best_of: 1 });
+        params.set_max_len(1);
+        params.set_split_on_word(true);
+
+        match state.full(params, data.as_ref()) {
+            Ok(0) => {}
+            Ok(n) => return Err(WhisperError::GenericError(n)),
+            Err(e) => return Err(e),
+        };
+
+        let segments = state.full_n_segments()?;
+
+        (0..segments)
+            .map(|n| {
+                let start = state.full_get_segment_t0(n)?;
+                let end = state.full_get_segment_t1(n)?;
+
+                let token_segs: Vec<Segment> = {
+                    let mut out = Vec::new();
+                    let n_tokens = state.full_n_tokens(n)? as i64;
+                    // TODO fix this by filtering out meta first and collecting, then we have the
+                    // actual n_tokens value to do math with.
+                    let non_meta_tokens = (0..n_tokens)
+                        .filter(|i| {
+                            let text = state.full_get_token_text(n, *i as i32).unwrap();
+                            !text.starts_with("[_")
+                        })
+                        .enumerate()
+                        .collect_vec();
+                    let n_non_meta_tokens = non_meta_tokens.len() as i64;
+
+                    for (i, original_i) in non_meta_tokens {
+                        let i = i as i64;
+                        let text = state.full_get_token_text(n, original_i as i32)?;
+                        // we're evenly spacing t0 and t1 for each token in the segment
+                        let t0 = start + (end - start) * i / n_non_meta_tokens;
+                        let t1 = start + (end - start) * (i + 1) / n_non_meta_tokens;
+                        out.push(Segment {
+                            start: t0.try_into().unwrap(),
+                            end: t1.try_into().unwrap(),
+                            text,
+                        });
+                    }
+                    out
+                };
+
+                Ok(token_segs)
+            })
+            .flatten_ok()
+            .collect()
+    }
+}
