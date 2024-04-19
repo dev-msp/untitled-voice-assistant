6cb472042d41fc0c9fbb63a7396f8d8f2a78e6d0
Matt Palmer <palmer.s.matt@gmail.com>
2024-04-19 11:55:37 -0400

    refactor(audio::input): move the list-channels subcommand impl here

--- src/audio/input.rs
+++ src/audio/input.rs
@@ -375,3 +375,29 @@ impl<O: MySample> Processor<f32, O> {
         Ok(())
     }
 }
+
+pub fn list_channels() -> Result<(), anyhow::Error> {
+    let host = cpal::default_host();
+    for (device_name, config) in host.input_devices()?.flat_map(|d| {
+        let name = d.name().expect("failed to get device name");
+        d.supported_input_configs()
+            .unwrap()
+            .map(move |c| (name.clone(), c))
+    }) {
+        let (buf_floor, buf_ceil) = match config.buffer_size() {
+            cpal::SupportedBufferSize::Range { min, max } => (min, max),
+            cpal::SupportedBufferSize::Unknown => unimplemented!(),
+        };
+        println!(
+            "{}: sample_rate:{}-{}, sample_format:{:?}, channels:{}, buffer_size: {}-{}",
+            device_name,
+            config.min_sample_rate().0,
+            config.max_sample_rate().0,
+            config.sample_format(),
+            config.channels(),
+            buf_floor,
+            buf_ceil
+        );
+    }
+    Ok(())
+}
